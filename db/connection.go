ce03cec3e2708a3401c1121c6948b57e56e6e3c0
David Carmelo Alvarado Rodríguez <160802539+ARDAV1D@users.noreply.github.com>
2024-05-29 14:33:51 -0600

    Add files via upload

---
+++ db/connection.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DBConnection() {
+	DSN := os.Getenv("DATABASE_URL")
+	if DSN == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+	var err error
+	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("DB connected")
+	}
+}
